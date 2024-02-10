10f1bdc7e904441814c3f76c25e52983710a0e78
irsyaad06 <irsyadfatahilah@gmail.com>
2024-02-10 15:49:02 +0700

    Docs : add Swisnl plugins

--- app/Providers/Filament/OwnerPanelProvider.php
+++ app/Providers/Filament/OwnerPanelProvider.php
@@ -17,6 +17,8 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Swis\Filament\Backgrounds\FilamentBackgroundsPlugin;
+use Swis\Filament\Backgrounds\ImageProviders\MyImages;
 
 class OwnerPanelProvider extends PanelProvider
 {
@@ -38,12 +40,12 @@ class OwnerPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
-                Pages\Dashboard::class,
+                // Pages\Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\AccountWidget::class,
+                // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -63,6 +65,13 @@ class OwnerPanelProvider extends PanelProvider
             // ->brandLogo(asset('images/logo.png'))
             ->favicon(asset('images/logo.png'))
             ->darkMode(false)
-            ->font('Poppins');
+            ->font('Poppins')
+            ->plugins([
+                FilamentBackgroundsPlugin::make()
+                    ->imageProvider(
+                        MyImages::make()
+                            ->directory('images/backgrounds')
+                    ),
+            ]);
     }
 }
